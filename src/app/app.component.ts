3be41f6287daa801c6cdcca8edc99125ebaffa85
Ayesha M <ayesha.m@beinex.com>
2024-05-01 11:58:51 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ToDoList';
+  todos: any[] = [];
+  newTodoText: string = '';
+  showEmptyMessage: boolean = false;
+
+  addTodo() {
+    if (this.newTodoText.trim()) {
+      this.todos.push({
+        text: this.newTodoText,
+        completed: false
+      });
+      this.newTodoText = '';
+      this.showEmptyMessage = false; // Hide the message when input is valid
+    } else {
+      this.showEmptyMessage = true; // Show the message when input is empty
+    }
+  }
+
+  // In your component class
+  deleteTodo(todo: any) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
 }
